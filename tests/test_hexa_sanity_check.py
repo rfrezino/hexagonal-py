7befaa4d74628a04a423fe8c06b02d161c9ae0fa
rfrezino <rodrigofrezino@gmail.com>
2021-11-15 18:58:16 +0100

    fix old tests

--- tests/test_hexa_sanity_check.py
+++ tests/test_hexa_sanity_check.py
@@ -4,6 +4,7 @@ from services.hexagonal_composition import HexagonalComposition
 from domain.hexagonal_error import HexagonalError
 from domain.hexagonal_layer import HexagonalLayer
 from use_cases.check_project_sanity_usecase import HexagonalSanityCheck
+from use_cases.generate_diagram_usecase import GenerateDiagramUseCase
 
 
 class HexagonalSanityCheckUnitTest(TestCase):
@@ -38,13 +39,15 @@ class HexagonalSanityCheckUnitTest(TestCase):
         errors = checker.check(composition=hexagonal_composition, source_folder='./test_projects/correct_project/')
         self.assertEqual(len(errors), 0)
 
-    # def test_generate_diagram(self):Î
-    #     infrastructure_layer = HexagonalLayer(name='infrastructure', directories=['infrastructure'])
-    #     use_cases_layer = HexagonalLayer(name='use_cases', directories=['usecases'])
-    #     services_layer = HexagonalLayer(name='services', directories=['services'])
-    #     domain_layer = HexagonalLayer(name='domain', directories=['domain'])
-    #
-    #     hexagonal_composition = HexagonalComposition()
-    #     hexagonal_composition + infrastructure_layer >> use_cases_layer >> services_layer >> domain_layer
-    #
-    #     self.assertTrue(generate_diagram())
+    def test_generate_diagram(self):
+        infrastructure_layer = HexagonalLayer(name='infrastructure', directories=['infrastructure'])
+        use_cases_layer = HexagonalLayer(name='use_cases', directories=['usecases'])
+        services_layer = HexagonalLayer(name='services', directories=['services'])
+        domain_layer = HexagonalLayer(name='domain', directories=['domain'])
+
+        hexagonal_composition = HexagonalComposition()
+        hexagonal_composition + infrastructure_layer >> use_cases_layer >> services_layer >> domain_layer
+
+        diagram = GenerateDiagramUseCase()
+
+        self.assertTrue(diagram.execute(hexagonal_composition=hexagonal_composition))
