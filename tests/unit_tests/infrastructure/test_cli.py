5857fe66d5f4ab732c52b601f41cbe1b09ab4b5d
rfrezino <rodrigofrezino@gmail.com>
2021-11-28 16:01:13 +0100

    Fix tests base path

--- tests/unit_tests/infrastructure/test_cli.py
+++ tests/unit_tests/infrastructure/test_cli.py
@@ -3,13 +3,14 @@ from unittest import TestCase
 from click.testing import CliRunner
 
 from infrastructure.cli import run_check
+from tests.utils import get_project_path
 
 
 class TestCli(TestCase):
     def test_cli_bootstrap_should_return_true(self):
         # This tests check the consistency of this on project
         runner = CliRunner()
-        result = runner.invoke(run_check, ['--source_path', '../../../src'])
+        result = runner.invoke(run_check, ['--source_path', get_project_path() + '/src'])
 
         self.assertEqual(result.exit_code, 0)
         self.assertIn('No errors found', result.output)
---
+++ tests/utils.py
@@ -0,0 +1,2 @@
+def get_project_path() -> str:
+    return __file__.split('/tests')[0]
