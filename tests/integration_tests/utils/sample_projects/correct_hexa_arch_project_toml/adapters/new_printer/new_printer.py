90ccc7988636c1bba387d5dbe9318c67e0ce21cf
rodrigo.rezino <rodrigo.rezino@freshbooks.com>
2022-01-17 10:06:20 +0100

    Create project test for toml configuration

---
+++ tests/integration_tests/utils/sample_projects/correct_hexa_arch_project_toml/adapters/new_printer/new_printer.py
@@ -0,0 +1,7 @@
+from tests.integration_tests.utils.sample_projects.correct_hexa_arch_project.domain.file_content import FileContent
+from tests.integration_tests.utils.sample_projects.correct_hexa_arch_project.port.printer import PrinterInterface
+
+
+class NewPrinter(PrinterInterface):
+    def print_file(self, file: FileContent):
+        print(file.content)
---
+++ tests/integration_tests/utils/sample_projects/correct_hexa_arch_project_toml/domain/file_content.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FileContent:
+    content: str
